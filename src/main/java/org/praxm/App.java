f202ba6ac6e59c99fc6164cf631cf0df7dc85225
praxm <praveen.kgec@gmail.com>
2022-07-14 01:16:47 +0530

    intial commit

---
+++ src/main/java/org/praxm/App.java
@@ -0,0 +1,13 @@
+package org.praxm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
